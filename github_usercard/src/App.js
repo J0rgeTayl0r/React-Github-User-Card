4659e114a0d741e2d2da6cac329902176cc661e7
carlo rothschild <carlorothschild@thors-hammer.home>
2020-05-05 23:55:43 -0400

    MVP

--- github_usercard/src/App.js
+++ github_usercard/src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 import FirstCard from "./components/Cards";
-import CardContent from './components/CardContent';
+import CardContent from "./components/CardContent";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       data: [],
-      followers: []
+      followers: [],
     };
-
   }
   componentDidMount() {
     axios
@@ -20,17 +19,16 @@ class App extends React.Component {
     axios
       .get(`https://api.github.com/users/J0rgeTayl0r/followers`)
       .then((res) => this.setState({ followers: res.data }))
-      .catch((err) => console.log('err', err)); 
+      .catch((err) => console.log("err", err));
   }
   render() {
-    console.log('Data:',this.state.data, 'followers:', this.state.followers)
+    console.log("Data:", this.state.data, "followers:", this.state.followers);
     return (
-      <div>
+      <div className='main'>
         <FirstCard cardProps={this.state.data} />
         <CardContent cardConProps={this.state.followers} />
       </div>
     );
   }
-  
 }
 export default App;
